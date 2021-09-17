d3d250eb4b5edeb97d875fc76fbf3bd33ef59769
Max Maldonado <nuup20@gmail.com>
2021-09-17 17:19:35 -0600

    initial commit

---
+++ build-Cotorro-Desktop_Qt_6_1_3_MSVC2019_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,8 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "F:/Programs/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "F:/Programs/Qt/6.1.3/msvc2019_64/bin/qmake.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "F:/Programs/Qt/6.1.3/msvc2019_64" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/HostX64/x64/cl.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/HostX64/x64/cl.exe" CACHE "STRING" "" FORCE)
