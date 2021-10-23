66f033cc154e3d4972bc99aca10460052b19e4f1
Max Maldonado <nuup20@gmail.com>
2021-10-23 14:20:40 -0600

    feat: Transformable node

--- Cotorro/ctTimeLineLine.cpp
+++ Cotorro/ctTimeLineLine.cpp
@@ -9,9 +9,21 @@
 namespace ct {
 
 TimeLineLine::TimeLineLine() :
+  TransformableNode(),
   _m_height(100),
-  _m_position(),
   _m_text()
+{
+  return;
+}
+
+TimeLineLine::~TimeLineLine()
+{
+  destroy();
+  return;
+}
+
+void
+TimeLineLine::onDestroy()
 {
   return;
 }
@@ -40,8 +52,6 @@ TimeLineLine::init()
   _m_text.setString("0:00");
   _m_text.setCharacterSize(12);
   _m_text.setFillColor(sf::Color::Black);
-
-  _updateLine();
   return;
 }
 
@@ -55,17 +65,28 @@ TimeLineLine::setLabel(const QString &_string)
 void
 TimeLineLine::update(sf::RenderWindow &_window)
 {
+  // Calculate vertices.
+  _m_vertices[0].position.x = 0.0f;
+  _m_vertices[0].position.y = 0.0f;
+
+  _m_vertices[1].position.x = 0.0f;
+  _m_vertices[1].position.y = _m_height;
+
+  sf::FloatRect globalBounds = _m_text.getGlobalBounds();
+  _m_text.setPosition(
+      _m_vertices[1].position.x + 3.0f,
+      _m_vertices[1].position.y - globalBounds.height * 2.0f
+  );
+
+  // Transform vertices.
+  _updateTransform();
+  _m_vertices[0].position = _m_global.transformPoint(_m_vertices[0].position);
+  _m_vertices[1].position = _m_global.transformPoint(_m_vertices[1].position);
+  _m_text.setPosition(_m_global.transformPoint(_m_text.getPosition()));
+
+  // Draw Vertices
   _window.draw(_m_vertices, 2, sf::Lines);
   _window.draw(_m_text);
-  return;
-}
-
-void
-TimeLineLine::setPosition(const float &_x, const float &_y)
-{
-  _m_position.x = _x;
-  _m_position.y = _y;
-  _updateLine();
   return;
 }
 
@@ -73,26 +94,8 @@ void
 TimeLineLine::setHeight(const float &_height)
 {
   _m_height = _height;
-  _updateLine();
   return;
 }
 
-void
-TimeLineLine::_updateLine()
-{
-  _m_vertices[0].position.x = _m_position.x;
-  _m_vertices[0].position.y = _m_position.y;
-
-  _m_vertices[1].position.x = _m_position.x;
-  _m_vertices[1].position.y = _m_position.y + _m_height;
-
-  sf::FloatRect globalBounds = _m_text.getGlobalBounds();
-  _m_text.setPosition(
-      _m_vertices[1].position.x + 3.0f,
-      _m_vertices[1].position.y - globalBounds.height * 2
-  );
-
-  return;
-}
 }
 
---
+++ Cotorro/ctTransformableNode.cpp
@@ -0,0 +1,80 @@
+#include "ctTransformableNode.h"
+
+#include <QUuid>
+
+namespace ct {
+
+TransformableNode::TransformableNode() :
+  _m_uuid(QUuid::createUuid().toString(QUuid::StringFormat::Id128)),
+  _m_pParent(nullptr),
+  _m_hChildren(),
+  _m_isDirty(true)
+{
+  return;
+}
+
+TransformableNode::~TransformableNode()
+{
+  return;
+}
+
+void
+TransformableNode::onDestroy()
+{
+  // Implement in derived class.
+  return;
+}
+
+void
+TransformableNode::addChild(TransformableNode *_transformableNode)
+{
+  QString uuid = _transformableNode->getUuid();
+  if(_m_hChildren.find(uuid) != _m_hChildren.end()) {
+    return;
+  }
+
+  _m_hChildren.insert(uuid, _transformableNode);
+  return;
+}
+
+const sf::Transform&
+TransformableNode::getGlobalTransform()
+{
+  _updateTransform();
+  return _m_global;
+}
+
+QString
+TransformableNode::getUuid()
+{
+  return _m_uuid;
+}
+
+void
+TransformableNode::destroy()
+{
+  onDestroy();
+
+  foreach(TransformableNode* _object, _m_hChildren) {
+    _object->destroy();
+  }
+
+  qDeleteAll(_m_hChildren);
+  _m_hChildren.clear();
+  return;
+}
+
+void
+TransformableNode::_updateTransform()
+{
+  if(_m_pParent != nullptr) {
+    _m_global.combine(_m_pParent->getGlobalTransform());
+  }
+  else {
+    _m_global = getTransform();
+  }
+  return;
+}
+
+}
+
